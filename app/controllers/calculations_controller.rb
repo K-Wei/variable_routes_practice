dfae58f0eb980ae2b4718e882f2921d34b854bfa
K-Wei <kevinwei@users.noreply.github.com>
2017-01-26 14:17:24 -0600

    finished square, random, and square root form
    worked on variable routes

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,38 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+
+    @num = params["the_number"].to_f
+
+    @squared = @num.to_f ** 2
+
+    render("calculations/square.html.erb")
+  end
+
+  def random
+
+    @min_bound = params["min_bound"].to_f
+    @max_bound = params["max_bound"].to_f
+    @rand = rand(@min_bound..@max_bound)
+
+  end
+
+  def sqrt_form
+
+    render("calculations/sqrt_form.html.erb")
+
+  end
+
+  def sqrt
+
+    @num = params["num"].to_f
+
+    @sqrt = @num.to_f ** 0.5
+
+    render("calculations/sqrt_form.html.erb")
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square/:the_number", { :controller => "calculations", :action => "square"})
+
+  get("/random/:min_bound/:max_bound", { :controller => "calculations", :action => "random"})
+
+  get("/square_root_form/", { :controller => "calculations", :action => "sqrt_form"})
+
+  get("/calculate_square_root/", { :controller => "calculations", :action => "sqrt"})
+
+  # get("/payment/")
 end
